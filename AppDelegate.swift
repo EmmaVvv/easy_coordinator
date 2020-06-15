459c6298b9f84b675e195f5e3be16e2993698de8
Emma <53192422+EmmaVvv@users.noreply.github.com>
2020-06-15 19:50:11 +0400

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var coordinator: MainCoordinator?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupProjectNavigation()
+        return true
+    }
+}
+
+extension AppDelegate {
+    private func setupProjectNavigation() {
+        let navigationController = UINavigationController()
+        coordinator = MainCoordinator(navigationController: navigationController)
+        coordinator?.start()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
